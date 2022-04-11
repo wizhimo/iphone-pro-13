fec9c6299ee3ae045d288120bbe85ddf417b6c14
hishaam ahmad <wizhimo95@gmail.com>
2022-04-11 00:11:15 -0600

    added some features

---
+++ main.js
@@ -0,0 +1,11 @@
+let mainImg = document.querySelector('.mainImg');
+let container = document.querySelector('.container');
+
+
+function phones(src) {
+    mainImg.src = src;
+}
+
+function colors(color) {
+    container.style.background = color;
+}
