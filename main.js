af90067c1996f00da3355e886d24d6f4694baf17
hishaam ahmad <wizhimo95@gmail.com>
2022-04-12 23:43:14 -0600

    added responsive features

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 let mainImg = document.querySelector('.mainImg');
 let container = document.querySelector('.container');
+let myUl = document.querySelector('.myUl');
+let mobileBurger = document.querySelector('.mobileMenu');
+
+
 
 
 function phones(src) {
@@ -9,3 +13,8 @@ function phones(src) {
 function colors(color) {
     container.style.background = color;
 }
+
+mobileBurger.addEventListener('click', () => {
+    myUl.classList.toggle("menuDrop");
+
+})
