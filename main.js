39ad1d25cee5cec82979f31c6429c74363526da1
hishaam ahmad <wizhimo95@gmail.com>
2022-04-13 22:10:53 -0600

    added animation to menu burger

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ let container = document.querySelector('.container');
 let myUl = document.querySelector('.myUl');
 let mobileBurger = document.querySelector('.mobileMenu');
 
+let row1 = document.querySelector('.row1');
+let row2 = document.querySelector('.row2');
+let row3 = document.querySelector('.row3');
 
 
 
@@ -16,5 +19,8 @@ function colors(color) {
 
 mobileBurger.addEventListener('click', () => {
     myUl.classList.toggle("menuDrop");
+    row2.classList.toggle("row2After");
+    row1.classList.toggle("row1After");
+    row3.classList.toggle("row3After");
 
 })
