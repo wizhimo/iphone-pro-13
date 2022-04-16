9b90e2e82283bcc87a4a25037010a59feff16e75
hishaam ahmad <wizhimo95@gmail.com>
2022-04-15 23:53:13 -0600

    added id number to products in data.js file

--- data.js
+++ data.js
@@ -1,24 +1,29 @@
 export let products = [{
+        id: 1,
         name: 'iphone 13 pro',
         price: '$1000',
         image: './images/0.png'
     },
     {
+        id: 2,
         name: 'iphone 13 pro',
         price: '$1100',
         image: './images/1.png'
     },
     {
+        id: 3,
         name: 'iphone 13 pro',
         price: '$1099',
         image: './images/2.png'
     },
     {
+        id: 4,
         name: 'iphone 13 pro',
         price: '$1000',
         image: './images/3.png'
     },
     {
+        id: 5,
         name: 'iphone 13 pro',
         price: '$1200',
         image: './images/4.png'
